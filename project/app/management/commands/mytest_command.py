5e1b3fd4e408b2df01d7c0411acda2bcdc74a4c4
stanislavn <29331439+stanislavn@users.noreply.github.com>
2025-03-06 23:35:57 +0100

    yo

--- project/app/management/commands/mytest_command.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "A test management command"
-
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Hello from the management command!")
--- setup-rclone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p /root/.config/rclone
-envsubst < /root/.config/rclone/rclone.conf.template > /root/.config/rclone/rclone.conf
-chmod 600 /root/.config/rclone/rclone.conf
-exec "$@"
